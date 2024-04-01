72f6f4f07f50cf6b2590f48dc93ef667e4c2b00d
yonatanlla <yonatanllanto174@gmail.com>
2024-04-01 12:30:57 -0500

    terminando navbr

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,18 +8,18 @@ const NavBar = () => {
   const location = useLocation()
   return (
     <div className='flex justify-center dark:bg-neutral-950  '>
-      <div className='max-w-6xl w-full flex justify-between'>
+      <div className='max-w-6xl w-full flex justify-between m-2'>
         <div className=''>
           {/* <Logo theme={'light'}/> */}
           <LogoSun />
         </div>
         <div>
-          <ul className='flex w-48 justify-between items-center h-full'>
-            <Link to="/" className={location.pathname === "/" ? `transition-all duration-300 border-b-8 border-[#ff3399]  dark:text-white hover:text-[#ff3399] w-full h-full flex justify-center items-center` : " w-full h-full dark:text-white flex justify-center items-center"}>
-              <li>Servicios</li>
+          <ul className='flex w-64 justify-between items-center h-full'>
+            <Link to="/" className={location.pathname === "/" ? `transition-all hover:text-[#ff3399]  duration-100 border-b-4 border-[#ff3399]  dark:text-[#ff3399]  w-full h-full flex justify-center items-center` : " w-full h-full transition-all  duration-100 dark:hover:text-[#ff2190] dark:text-white flex justify-center items-center"}>
+              <li className=' text-body-bold'>Servicios</li>
             </Link>
-            <Link to="/proyectos" className='transition-all duration-300 hover:border-b-8 dark:text-white hover:border-[#ff3399] w-full h-full flex justify-center items-center'>
-              <li>Proyectos</li>
+            <Link to="/proyectos" className={location.pathname === "/proyectos" ? `transition-all duration-100 border-b-4 border-[#ff3399] dark:text-[#ff3399] hover:text-[#ff3399] w-full h-full flex justify-center items-center` : " w-full h-full dark:text-white flex justify-center items-center"}>
+              <li className=' text-body-bold'>Proyectos</li>
             </Link>
           </ul>
         </div>
--- src/components/Themes.tsx
+++ src/components/Themes.tsx
@@ -22,7 +22,7 @@ const Themes = () => {
     } else {
       document.querySelector('html')?.classList.remove('dark')
     }
-    
+
   }, [theme])
   const handleChangeTheme = () => {
     setTheme(prevTheme => prevTheme === "light" ? "dark" : "light")
@@ -31,16 +31,17 @@ const Themes = () => {
 
   return (
     <>
-       <div className='flex justify-center items-center'>
-      <button
-        className={`relative w-16 h-8 bg-neutral-800 rounded-full p-1 `}
-        onClick={handleChangeTheme}
-      >
-        <span className={`block absolute top-0 left-0 w-8 h-8 bg-[#ff3399] rounded-full shadow-md transform transition-transform ${theme === "light" ? "translate-x-full" : ""}`} />
-        <MdSunny className={`absolute left-2 top-2 text-yellow-500 ${theme === "light" ? "opacity-100" : "opacity-0"}`} />
-        <BsMoonStarsFill className={`absolute right-2 top-2 text-blue-500 ${theme === "dark" ? "opacity-100" : "opacity-0"}`} />
-      </button>
-    </div>
+      <div className='flex justify-center items-center'>
+        <button
+          className={`relative w-16 h-8 bg-neutral-800 rounded-full p-1 `}
+          onClick={handleChangeTheme}
+        >
+          <span className={`block absolute top-0 left-0 w-8 h-8 bg-[#ff3399] rounded-full
+                          shadow-md transform transition-transform ${theme === "light" ? "translate-x-full" : ""}`} />
+          <MdSunny className={`absolute left-2 top-2 text-yellow-500 ${theme === "light" ? "opacity-100" : "opacity-0"}`} />
+          <BsMoonStarsFill className={`absolute right-2 top-2 text-blue-500 ${theme === "dark" ? "opacity-100" : "opacity-0"}`} />
+        </button>
+      </div>
     </>
   )
 }
